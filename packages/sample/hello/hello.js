c5ca8e5103e9be397270b8e95ab4f96d4594547e
Travis Dumont <tdumont97566@gmail.com>
2024-08-06 21:58:24 -0500

    test commit

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,8 +1,12 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
-  }
+const main = async (args) => {
+  const url = "https://sample-expressjs-ydg38.ondigitalocean.app/";
 
-exports.main = main
+  await fetch(url).then((response) => console.log(response));
+
+  let name = args?.name || "stranger";
+  let greeting = "Hello " + name + "!";
+  console.log(greeting);
+  return { body: greeting };
+};
+main();
+exports.main = main;
